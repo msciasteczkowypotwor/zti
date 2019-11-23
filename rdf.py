dc8aa76ba90fb8dc2d919723f3145e70428c7f93
marcin <msciasteczkowypotwor@gmail.com>
2019-11-23 15:23:34 +0100

    i

--- rdf.py
+++ rdf.py
@@ -2,12 +2,13 @@ from rdflib import Graph
 import pprint
 import spacy
 import en_core_web_sm
+from pynif import NIFCollection
 
 #g=Graph()
 #g.parse("test.xml.ttl", format='ttl')
 
 #g.serialize('test.rdf', format='pretty-xml', max_depth=3)
-
+collection = NIFCollection(uri='http://freme-project.eu')
 g = Graph()
 g.load('test.xml.ttl', format='ttl')
 
@@ -21,3 +22,33 @@ for row in g.query('select ?s where { [] nif:isString ?s .}'):
         start = statement.find(X.text)
         stop = start+len(X.text)
         print(X.text, X.start_char, X.end_char,)
+
+
+
+
+
+context = collection.add_context(uri='http://freme-project.eu',
+                                 mention='Diego Maradona is from Argentina.')
+context.add_phrase(
+    beginIndex=0,
+    endIndex=14,
+    taClassRef=['http://dbpedia.org/ontology/SportsManager', 'http://dbpedia.org/ontology/Person', 'http://nerd.eurecom.fr/ontology#Person'],
+    score=0.9869992701528016,
+    annotator='http://freme-project.eu/tools/freme-ner',
+    taIdentRef='http://dbpedia.org/resource/Diego_Maradona',
+    taMsClassRef='http://dbpedia.org/ontology/SoccerManager')
+
+context.add_phrase(
+    beginIndex=23,
+    endIndex=32,
+    taClassRef=['http://dbpedia.org/ontology/PopulatedPlace', 'http://nerd.eurecom.fr/ontology#Location',
+    'http://dbpedia.org/ontology/Place'],
+    score=0.9804963628413852,
+    annotator='http://freme-project.eu/tools/freme-ner',
+    taMsClassRef='http://dbpedia.org/resource/Argentina')
+
+generated_nif = collection.dumps(format='turtle')
+file = open('b1.ttl', 'w')
+file.write(str(generated_nif))
+file.close()
+#print(generated_nif)
