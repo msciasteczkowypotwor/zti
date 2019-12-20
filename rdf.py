4c2df4074c9745701ce6f45b94f0daa99757d003
marcin <msciasteczkowypotwor@gmail.com>
2019-12-20 19:36:03 +0100

    a

--- rdf.py
+++ rdf.py
@@ -1,8 +1,8 @@
 import spacy
 import en_core_web_sm  # sudo python -m spacy download en_core_web_sm
-
 from rdflib import Graph
 from SPARQLWrapper import SPARQLWrapper, JSON
+
 import re
 
 FINDING_REF_CONTEXT = "^<.*>$"
@@ -44,35 +44,32 @@ def create_context(name):
     return '<http://aksw.org/notInWiki/' + name + '>'
 
 
-
-
-
 g = Graph()
-
-
-
-
-g.load('test.xml.ttl', format='ttl')
-
 spacy.nlp = en_core_web_sm.load()
 
-f = open('inputs/1.xml.ttl', 'r')
-output = f.read()
+
+def find_named_entitys(file_name):
+    g.load(file_name, format='ttl')
+    f = open(file_name, 'r')
+    output = f.read()
+    print(output)
+    full_context = find(output, FINDING_REF_CONTEXT)
+
+    for row in g.query('select ?s where { [] nif:isString ?s .}'):
+        statement = row.s
+        doc = spacy.nlp(str(statement))
+        for X in doc.ents:
+            a = X.text.replace(" ", "_")
+
+            if X.label_ == "PERSON" or X.label_ == "ORG" or X.label_ == "GPE":
+                start = statement.find(X.text)
+                stop = start + len(X.text)
+                print(X.text, X.label_, X.start_char, X.end_char)
+                output += "\n" + create_note(X.text, str(start), str(stop), create_context(a), full_context)
+    return output
 
 
-full_context = find(output, FINDING_REF_CONTEXT)
+f = open('outputs/4a.xml.ttl','w')
+f.write(find_named_entitys("inputs/4.xml.ttl"))
 
 
-for row in g.query('select ?s where { [] nif:isString ?s .}'):
-    statement = row.s
-    doc = spacy.nlp(str(statement))
-    for X in doc.ents:
-        a = X.text.replace(" ", "_")
-
-        if X.label_ == "PERSON" or X.label_ == "ORG" or X.label_ == "GPE":
-            start = statement.find(X.text)
-            stop = start + len(X.text)
-            print(X.text, X.label_, X.start_char, X.end_char)
-            output += create_note(X.text, str(start), str(stop), create_context(a), full_context) +"\n"
-
-print(output)
